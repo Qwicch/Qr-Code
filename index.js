c564c59bd1bc1d4c0b800f95f150fd3757b11390
Ilmir Kireev <ilmire.kireev@gmail.com>
2024-03-01 22:53:15 -0500

    complete writeFile module

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 import inquirer from 'inquirer';
+const fs = require('node:fs');
 
 inquirer
   .prompt([
@@ -11,6 +12,15 @@ inquirer
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
+    fs.writeFile("url.txt", answers, (err) => {
+        if (err)
+          console.log(err);
+        else {
+          console.log("File written successfully\n");
+          console.log("The written has the following contents:");
+          console.log(fs.readFileSync("books.txt", "utf8"));
+        }
+      });
   })
   .catch((error) => {
     if (error.isTtyError) {
